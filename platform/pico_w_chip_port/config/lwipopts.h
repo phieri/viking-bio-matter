d56dd1553ef7f067a9e30535bd2af55a0908f782
Philip Eriksson <12006381+phieri@users.noreply.github.com>
2026-02-24 00:07:30 +0000

    Edit lwipopts.h for improved memory settings

--- platform/pico_w_chip_port/config/lwipopts.h
+++ platform/pico_w_chip_port/config/lwipopts.h
@@ -1,22 +1,20 @@
-/**
- * @file lwipopts.h
- * @brief lwIP configuration for Viking Bio Matter Bridge (Pico W)
- * 
- * Based on Pico SDK lwIP defaults for WiFi applications.
- * Vendored to resolve configuration header access issues.
- */
-
 #ifndef LWIPOPTS_H
 #define LWIPOPTS_H
 
-// Platform specifics
 #define NO_SYS                      1
-#define LWIP_SOCKET                 0
-#define LWIP_NETCONN                0
 
-// Memory options - DO NOT use MEM_LIBC_MALLOC with threadsafe_background
+#define LWIP_SOCKET                 0
+
+#if PICO_CYW43_ARCH_POLL
+#define MEM_LIBC_MALLOC             1
+#else
+// MEM_LIBC_MALLOC is incompatible with non polling versions
 #define MEM_LIBC_MALLOC             0
-#define MEM_SIZE                    (8 * 1024)
+#endif
+#define MEM_ALIGNMENT               4
+#ifndef MEM_SIZE
+#define MEM_SIZE                    4000
+#endif
 #define MEMP_NUM_TCP_SEG            32
 #define MEMP_NUM_ARP_QUEUE          10
 #define PBUF_POOL_SIZE              24
@@ -24,8 +22,8 @@
 #define LWIP_ETHERNET               1
 #define LWIP_ICMP                   1
 #define LWIP_RAW                    1
-#define TCP_MSS                     1460
 #define TCP_WND                     (8 * TCP_MSS)
+#define TCP_MSS                     1460
 #define TCP_SND_BUF                 (8 * TCP_MSS)
 #define TCP_SND_QUEUELEN            ((4 * (TCP_SND_BUF) + (TCP_MSS - 1)) / (TCP_MSS))
 #define LWIP_NETIF_STATUS_CALLBACK  1
@@ -36,12 +34,12 @@
 #define SYS_STATS                   0
 #define MEMP_STATS                  0
 #define LINK_STATS                  0
-
-// DHCP options
+// #define ETH_PAD_SIZE                2
+#define LWIP_CHKSUM_ALGORITHM       3
 #define LWIP_DHCP                   1
-#define LWIP_IPV4                   1
+#define LWIP_IPV4                   0
 #define LWIP_IPV6                   1
-#define LWIP_TCP                    1
+#define LWIP_TCP                    0
 #define LWIP_UDP                    1
 #define LWIP_DNS                    1
 #define LWIP_TCP_KEEPALIVE          1
@@ -49,28 +47,39 @@
 #define DHCP_DOES_ARP_CHECK         0
 #define LWIP_DHCP_DOES_ACD_CHECK    0
 
-// mDNS options for Matter device discovery
-#define LWIP_MDNS_RESPONDER         1
-#define LWIP_IGMP                   1
-#define LWIP_NUM_NETIF_CLIENT_DATA  2
+#ifndef NDEBUG
+#define LWIP_DEBUG                  1
+#define LWIP_STATS                  1
+#define LWIP_STATS_DISPLAY          1
+#endif
 
-// Threading
-#define TCPIP_THREAD_STACKSIZE      1024
-#define DEFAULT_THREAD_STACKSIZE    1024
-#define DEFAULT_RAW_RECVMBOX_SIZE   8
-#define TCPIP_MBOX_SIZE             8
-#define LWIP_TIMEVAL_PRIVATE        0
+#define ETHARP_DEBUG                LWIP_DBG_OFF
+#define NETIF_DEBUG                 LWIP_DBG_OFF
+#define PBUF_DEBUG                  LWIP_DBG_OFF
+#define API_LIB_DEBUG               LWIP_DBG_OFF
+#define API_MSG_DEBUG               LWIP_DBG_OFF
+#define SOCKETS_DEBUG               LWIP_DBG_OFF
+#define ICMP_DEBUG                  LWIP_DBG_OFF
+#define INET_DEBUG                  LWIP_DBG_OFF
+#define IP_DEBUG                    LWIP_DBG_OFF
+#define IP_REASS_DEBUG              LWIP_DBG_OFF
+#define RAW_DEBUG                   LWIP_DBG_OFF
+#define MEM_DEBUG                   LWIP_DBG_OFF
+#define MEMP_DEBUG                  LWIP_DBG_OFF
+#define SYS_DEBUG                   LWIP_DBG_OFF
+#define TCP_DEBUG                   LWIP_DBG_OFF
+#define TCP_INPUT_DEBUG             LWIP_DBG_OFF
+#define TCP_OUTPUT_DEBUG            LWIP_DBG_OFF
+#define TCP_RTO_DEBUG               LWIP_DBG_OFF
+#define TCP_CWND_DEBUG              LWIP_DBG_OFF
+#define TCP_WND_DEBUG               LWIP_DBG_OFF
+#define TCP_FR_DEBUG                LWIP_DBG_OFF
+#define TCP_QLEN_DEBUG              LWIP_DBG_OFF
+#define TCP_RST_DEBUG               LWIP_DBG_OFF
+#define UDP_DEBUG                   LWIP_DBG_OFF
+#define TCPIP_DEBUG                 LWIP_DBG_OFF
+#define PPP_DEBUG                   LWIP_DBG_OFF
+#define SLIP_DEBUG                  LWIP_DBG_OFF
+#define DHCP_DEBUG                  LWIP_DBG_OFF
 
-// Checksums - let hardware handle if possible  
-#define CHECKSUM_GEN_IP             0
-#define CHECKSUM_GEN_UDP            0
-#define CHECKSUM_GEN_TCP            0
-#define CHECKSUM_GEN_ICMP           0
-#define CHECKSUM_GEN_ICMP6          0
-#define CHECKSUM_CHECK_IP           0
-#define CHECKSUM_CHECK_UDP          0
-#define CHECKSUM_CHECK_TCP          0
-#define CHECKSUM_CHECK_ICMP         0
-#define CHECKSUM_CHECK_ICMP6        0
-
-#endif /* LWIPOPTS_H */
+#endif /* __LWIPOPTS_H__ */
